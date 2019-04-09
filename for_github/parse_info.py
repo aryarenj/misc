24daf947e7dcebb68155dcb4731dd809e6ef5d84
Arya Renjan <31288479+aryarenj@users.noreply.github.com>
2019-04-08 20:22:48 -0400

    Add files via upload

--- for_github/parse_info.py
+++ for_github/parse_info.py
@@ -1,27 +1,6 @@
-from mitmproxy import io, http
-#from mitmproxy.io import FlowReader
-from mitmproxy.exceptions import FlowReadException
-
-
-fileout = open("out.txt", 'w' )
-
-def get_info(flow):
-
-        fileout.write(flow.server_conn.ip_address + "\n")
-        fileout.write(flow.server_conn.sni + "\n")
-        fileout.write(flow.server_conn.id + "\n")
-        fileout.write(flow.server_conn.address + "\n")
-        fileout.write(flow.server_conn.source_address + "\n")
-        fileout.write(flow.server_conn.tls_version + "\n") 
-        fileout.write(flow.client_conn.address + "\n")
-        fileout.write(flow.client_conn.id + "\n")
-        fileout.write(flow.client_conn.sni + "\n") 
-        fileout.write(flow.client_conn.tls_version + "\n") 
-        fileout.write(flow.client_conn.cipher_name + "\n") 
-        fileout.write(flow.client_conn.clientcert + "\n")
-        fileout.write (dir(flow.client_conn + "\n") + "\n")
-        
-        fileout.write (dir(flow.server_conn + "\n") + "\n")
-
-
+def response(flow):
+        print ("Arya Hello")
+        print(flow.server_conn.sni )
+        print (flow.server_conn.ip_address[0])
+        print (flow.server_conn.source_address[0])
 
